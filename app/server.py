4bfc9dfd9425b452f0947110addca7bb1da6dab8
therishiagarwal <ra578021@gmail.com>
2025-08-30 22:06:20 +0530

    feat(app): add hello server and Dockerfile

---
+++ app/server.py
@@ -0,0 +1,13 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket, os
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        msg = f"Hello from {socket.gethostname()} | ENV={os.getenv('APP_ENV','dev')}\n"
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(msg.encode())
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", "8080"))
+    HTTPServer(("", port), Handler).serve_forever()
